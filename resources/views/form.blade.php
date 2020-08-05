517a3e44f059cdb10bd3672bec18e7a5b381c6c0
Khairun Nisak <just.yoen.ca@gmail.com>
2020-08-05 12:49:16 +0700

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>Buat Account Baru!</h1>
+        <h3>Sign Up Form</h3>
+        <!-- Membuat Form-->
+        <form action="welcome.html">
+            <label for="first_name">First Name:</label><br><br>
+            <input type="text" id="first_name"><br><br>
+
+            <label for="last_name">Last Name:</label><br><br>
+            <input type="text" id="last_name"><br><br>
+
+            <label>Gender:</label><br><br>
+            <input type="radio" name="gender" value="0"> Male<br>
+            <input type="radio" name="gender" value="1"> Female<br>
+            <input type="radio" name="gender" value="2"> Other<br>
+            <br><br>
+
+            <label>Nationality</label><br><br>
+            <select>
+                <option>Indonesian</option>
+                <option>Malaysian</option>
+                <option>Singapore</option>
+                <option>Australian</option>
+            </select>
+            <br><br>
+            <label>Language Spoken:</label><br><br>
+            <input type="checkbox" name="language" value="0"> Bahasa Indonesia<br>
+            <input type="checkbox" name="language" value="1"> English<br>
+            <input type="checkbox" name="language" value="2"> Other<br>
+            <br><br>
+
+            <label for="bio">Bio:</label><br><br>
+            <textarea cols="30" rows="7" id="bio"></textarea>
+            <br>
+            <input type="submit" value="Sign Up">
+        </form>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>SELAMAT DATANG!</h1>
+        <h3>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h3>
+    </body>
+</html>
